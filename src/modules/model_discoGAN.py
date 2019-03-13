27b90ca43ebd951129ff276844fb8485de7843fc
vinaykumar2491 <vinaykumar2491@gmail.com>
2019-03-13 07:19:54 -0400

    sessions and modules

---
+++ src/modules/model_discoGAN.py
@@ -0,0 +1,25 @@
+"""Implementation of original Disco-GAN architecture"""
+import os
+import numpy as np
+import fastai
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+__author__ = "Vinay Kumar"
+__copyright__ = "Vinay Kumar @2019"
+__license__ = "MIT"
+
+class Discriminator(torch.nn.Module):
+    def __init__(self):
+        pass
+
+    def forward():
+        pass
+
+class Generator(torch.nn.Module):
+    def __init__(self):
+        pass
+
+    def forward():
+        pass
