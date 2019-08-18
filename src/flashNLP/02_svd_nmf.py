8f009646fee03bd759125f802976dd2d7fdf156b
vinaykumar2491 <vinaykumar2491@gmail.com>
2019-08-18 22:00:25 +0530

    svd

--- src/flashNLP/02_svd_nmf.py
+++ src/flashNLP/02_svd_nmf.py
@@ -64,4 +64,61 @@ print(vocab[7000:7020])
 
 #########################################################################################
 ## Singular Value Decomposition
+"""
+1.  We would expect that the wordds that appear most frequently in one topic would appear
+    less frequently in other topics; otherwise that word wouldn't be a good choice to
+    separate out the two topics.
+    Hence, the matrix representing the topics are orthogonal.
+2.  The SVD algorithm factorizes a matrix into three different matrices:
+    a. matrix U: orthogonal columns
+    b. matrix E: diagonal matrix
+    c. matrix V: orthogonal rows
+3.  SVD is exact decomposition, since the matrices it generates are big enough to cover
+    the original matrix.
+
+Uses of SVD:
+a.  Latent Semantic Analysis (LSA)
+"""
+
+U, s, Vh = linalg.svd(vectors,full_matrices=False)
+print(U.shape, s.shape, Vh.shape)
+
+# note: s is a vector
+# tip: use np.diag to convert a vector into diagonal matrix and vice-versa
+print(s[:4])
+print(np.diag(s)[:4,:])
+print(np.diag(np.diag(s))[:4])
+
+# Confirm that U, s, Vh are decompositions of matrix "vectors"
+# tip: check the product of U, s, Vh to equate to "vectors"
+vectors_decomposed = U @ np.diag(s) @ Vh
+# print(vectors_decomposed - vectors)
+print(np.allclose(vectors_decomposed, vectors))
+
+# Confirm that U, Vh are orthogonal
+# tip: 1. Check if they are row-wise or col-wise orthogonal
+# tip: 2. Check if their norm is 1
+# tip: 3. U is col-wise orthonormal. Vh is row-wise
+
+print(np.allclose(U.T @ U, np.diag(np.ones(U.T.shape[0]))))
+print(np.allclose(Vh @ Vh.T, np.diag(np.ones(Vh.shape[0]))))
+
+print(np.allclose(U.T @ U, np.eye(U.shape[1])))
+print(np.allclose(Vh @ Vh.T, np.eye(Vh.shape[0])))
+
+# What can we say about the singular values s
+plt.plot(s)
+plt.show()
+
 #########################################################################################
+
+def show_topics(a: np.array):
+    top_words = lambda t: [vocab[i] for i in np.argsort(t)[:-num_top_words-1:-1]]
+    topic_words = ([top_words(t) for t in a])
+    return [" ".join(t) for t in topic_words]
+
+print(show_topics(Vh[:10]))
+
+#########################################################################################
+
+
