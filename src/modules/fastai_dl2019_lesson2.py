23337c1e22a571072d2dbcb0234022118ffdb11b
vinaykumar2491 <vinaykumar2491@gmail.com>
2019-09-13 08:09:36 +0530

    fastai: #2> google_ds,resnet

---
+++ src/modules/fastai_dl2019_lesson2.py
@@ -0,0 +1,10 @@
+"""
+Raw Python version of jupyter notebooks from fast.ai course
+"""
+
+__author__ = "Vinay Kumar"
+
+from fastai import *
+from fastai.vision import *
+
+def create_own_dataset_from_google():
