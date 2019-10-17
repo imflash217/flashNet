a22e4dd4f372d3f9b83081043a25e1ab06d86414
vinaykumar2491 <vinaykumar2491@gmail.com>
2019-10-17 19:45:24 +0530

    flashai DEV: run_notebook()

---
+++ src/flashai/run_notebook.py
@@ -0,0 +1,12 @@
+import nbformat
+import fire
+
+from nbconvert.preprocessors import ExecutePreprocessor
+
+def run_notebook(path):
+    nb = nbformat.read(open(path), as_version=nbformat.NO_CONVERT)
+    ExecutePreprocessor(timeout=600).preprocess(nb, {})
+    print("done")
+
+if __name__ == "__main__":
+    fire.Fire(run_notebook)
